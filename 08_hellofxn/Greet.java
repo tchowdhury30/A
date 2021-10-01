7654a285e1b5a180fbcf92c759a0d6d03f3ab22f
tchowdhury30 <90805264+tchowdhury30@users.noreply.github.com>
2021-09-30 20:36:53 -0400

    Update Greet.java

--- 08_hellofxn/Greet.java
+++ 08_hellofxn/Greet.java
@@ -1,6 +1,28 @@
+/*
+Tasnim Chowdhury
+APCS
+HW08: Refactor Freshie Zero
+2021-09-30
+*/
+
+/**
+DISCOVERIES
+I don't need to add print method before greet().
+
+UNRESOLVED QUESTIONS
+What does main and what does class do?
+What is "System.out...?"
+
+*/
+
 public class Greet {
-	public static void main(String[] args) {
-		System.out.println("No hablo queso");
-		}
+	public static void main (String [] args) {
+		greet("Tasnim");
+		greet("Faiza");
+		greet("Diana");
 	}
-	
+
+	public static void greet(String name) {
+		System.out.println("Hi " + name + ", what's up??" );
+	}
+}
