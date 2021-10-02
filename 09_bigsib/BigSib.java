a94177bc4acdd1339dc89efd5ece3682630eb5d9
tchowdhury30 <90805264+tchowdhury30@users.noreply.github.com>
2021-10-01 22:26:55 -0400

    Add files via upload

---
+++ 09_bigsib/BigSib.java
@@ -0,0 +1,13 @@
+/*
+Tasnim Chowdhury + Mary
+APCS
+HW09: What Are BigSibs Good For?
+2021-10-01
+*/
+
+public class BigSib {
+        public static void greet(String name){
+                System.out.println( "Hey there, " + name + ", what is up???");
+        }
+
+}
---
+++ 09_bigsib/Greet.java
@@ -0,0 +1,21 @@
+/*
+Tasnim Chowdhury + Mary
+APCS
+HW09: What Are BigSibs Good For?
+2021-10-01
+*/
+
+/**
+DISCOVERIES
+Python is an interpretive language while Java is a compiler language
+The closer a language is to hardware the harder it is for humans to understand (more abstract)
+UNRESOLVED QUESTIONS
+*/
+
+public class Greet {
+    public static void main( String[] args ) {
+        BigSib.greet("Tasnim");
+        BigSib.greet("Russell");
+        BigSib.greet("Abdullah");
+    }
+}
