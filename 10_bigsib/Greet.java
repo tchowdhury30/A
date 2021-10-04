a1eae15f99b034b80850fab6b7660bcd6dc1e5c8
tchowdhury30 <90805264+tchowdhury30@users.noreply.github.com>
2021-10-04 19:44:48 -0400

    Add files via upload

---
+++ 10_bigsib/Greet.java
@@ -0,0 +1,20 @@
+/*
+Period 8 (Tasnim, Abdullah, Russell)
+APCS
+HW010: Refactor Big Sib One
+2021-10-04
+*/
+
+/**
+DISCOVERIES
+
+UNRESOLVED QUESTIONS
+*/
+
+public class Greet {
+    public static void main( String[] args ) {
+        BigSib.greet("Tasnim");
+        BigSib.greet("Russell");
+        BigSib.greet("Abdullah");
+    }
+}
