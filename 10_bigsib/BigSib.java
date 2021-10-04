d97a7568262ca171a4c2158c476942688522d101
tchowdhury30 <90805264+tchowdhury30@users.noreply.github.com>
2021-10-04 19:44:06 -0400

    Update BigSib.java

---
+++ 10_bigsib/BigSib.java
@@ -0,0 +1,16 @@
+/*
+Period 8 (Tasnim, Abdullah, Russell)
+APCS
+HW010: Refactor Big Sib One
+2021-10-04
+*/
+
+
+public class BigSib {
+        public static void main(String[] args){
+        }
+        public static void main(String str){
+          return "Hey there, " + name + ", what is up???";
+        }
+
+}
