e32176cefa2a5f993430e44b3d705290446bc365
tchowdhury30 <90805264+tchowdhury30@users.noreply.github.com>
2021-10-06 20:18:44 -0400

    Update Greet.java

---
+++ 13_hatch/Greet.java
@@ -0,0 +1,14 @@
+/*
+Team TJ (Tasnim + Mary, John + Po)
+APCS
+HW13: Where do BigSibs Come From?
+2021-10-06
+*/
+
+public class Greet {
+  public static void main( String[] args ) {
+    BigSib richard = new BigSib("Word up");
+//    richard.setHelloMsg("Word up");
+    System.out.println(richard.greet("freshman"));
+  } //end main()
+} //end Greet
