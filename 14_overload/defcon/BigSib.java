277299c9f71f7a78d6677a16cecd476ea989e236
tchowdhury30 <90805264+tchowdhury30@users.noreply.github.com>
2021-10-07 21:04:52 -0400

    Add files via upload

---
+++ 14_overload/defcon/BigSib.java
@@ -0,0 +1,29 @@
+/*
+TER: Tasnim, Eric, Russell
+APCS
+HW14: Customize Your Creation
+2021-10-07
+*/
+
+
+/**
+DISCOVERIES
+
+UNRESOLVED QUESTIONS
+
+*/
+
+
+public class BigSib {
+  static String helloMsg;
+
+  public BigSib(String n) {
+    helloMsg = n;
+  }
+
+  public static String greet(String name){
+    return(helloMsg + " " + name);
+  }
+
+
+}
