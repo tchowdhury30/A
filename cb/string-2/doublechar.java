0251d987803ac17695edcbcad009815024a5aec6
tchowdhury30 <90805264+tchowdhury30@users.noreply.github.com>
2021-10-30 16:01:15 -0400

    Delete cb/string-2 directory

--- cb/string-2/doublechar.java
+++
@@ -1,8 +0,0 @@
-public String doubleChar(String str) {
-  String result = "";
-  for (int i = 0; i < str.length(); i++){
-    result += str.charAt(i);
-    result += str.charAt(i);
-  }
-  return result;
-}
