5841212bada272bbbe5c4444844dca1397554be0
tchowdhury30 <90805264+tchowdhury30@users.noreply.github.com>
2022-01-12 19:37:56 -0500

    Update MagpieRunner2.java

--- cobo/magpie/activity2/MagpieRunner2.java
+++ cobo/magpie/activity2/MagpieRunner2.java
@@ -14,11 +14,18 @@ public class MagpieRunner2
 	public static void main(String[] args)
 	{
 		Magpie2 maggie = new Magpie2();
-		
+
 		System.out.println (maggie.getGreeting());
 		Scanner in = new Scanner (System.in);
 		String statement = in.nextLine();
-		
+
+
+		while (((statement.trim()).length()) == 0)
+		{
+			System.out.println ("Say something, please.");
+		}
+
+
 		while (!statement.equals("Bye"))
 		{
 			System.out.println (maggie.getResponse(statement));
